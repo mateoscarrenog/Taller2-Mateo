8a327d1651e3bf66a940b88208002473a23fdf5a
Mateo Carreño <mateoscarrenog@gmail.com>
2025-02-02 18:37:29 -0500

    En este cambio se crea una variable de texto y posteriormente se imprime

---
+++ ArchivoTaller2.py
@@ -0,0 +1,4 @@
+#1.11. Introduzca cambios a su archivo, modifique al menos una l´ınea de c´odigo y agregue una línea más. Guarde (CTRL+s). 
+
+VariableEjemplo = "Cambio en el archivo"
+print(VariableEjemplo)
