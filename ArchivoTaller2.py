6d36f216acfe8811a2d571bc52ecbbcdd44812bf
Mateo Carreño <mateoscarrenog@gmail.com>
2025-02-02 20:00:24 -0500

    En este cambio se crea una nueva variable de ejemplo (float) y se imprime el tipo con la función "type"

--- ArchivoTaller2.py
+++ ArchivoTaller2.py
@@ -5,3 +5,6 @@ print(VariableEjemplo)
 
 VariableEjemplo2 = 10
 print(f"El número {VariableEjemplo2} es divisible entre 2")
+
+VariableEjemplo3 = 3.5
+print(f"El tipo de variable de esta variable es: {type(VariableEjemplo3)}")
